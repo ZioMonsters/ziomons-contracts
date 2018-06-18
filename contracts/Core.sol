b89fd2dbfb9f2c2a5090446b9220f8eb47230d81
Matteo Bonacini <matteo.bonna@gmail.com>
2018-06-18 12:05:51 +0200

    mods

--- contracts/Core.sol
+++ contracts/Core.sol
@@ -4,5 +4,18 @@ import "./Mortal.sol";
 import "./Interfaces.sol";
 
 contract Core is Mortal, ERC721, ERC165, ERC721Receiver {
+	struct Monster {
+		uint8 atk;
+		uint8 def;
+		uint8 spd;
+		uint8 lvl;
+		uint16 exp;
+		uint8 rarity;
+		uint8 energy;
+	}
 
+	Monster[] monsters;
+	mapping(uint256 => address) owner;
+	mapping(address => uint256) balance;
+	
 }
