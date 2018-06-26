0c8a4605ef8b2075b766a628ba514f20359fe922
Emanuele Caruso <ema.caruso2@gmail.com>
2018-06-26 15:15:20 +0000

    -div

--- contracts/SafeMath.sol
+++ contracts/SafeMath.sol
@@ -23,19 +23,6 @@ library SafeMath {
     return c;
   }
 
-  /**
-  * @dev Integer division of two numbers, truncating the quotient.
-  */
-  function div(uint256 a, uint256 b) internal pure returns (uint256) {
-    // assert(b > 0); // Solidity automatically throws when dividing by 0
-    // uint256 c = a / b;
-    // assert(a == b * c + a % b); //will never happen
-    return a / b;
-  }
-
-  /**
-  * @dev Subtracts two numbers, throws on overflow (i.e. if subtrahend is greater than minuend).
-  */
   function sub(uint256 a, uint256 b) internal pure returns (uint256) {
     require(b <= a);
     return a - b;
