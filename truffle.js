921ec8a293108ff7ede8cd0faa89a51a096945b0
Emanuele Caruso <ema.caruso2@gmail.com>
2018-06-26 15:30:34 +0000

    network added

--- truffle.js
+++ truffle.js
@@ -1,6 +1,6 @@
 module.exports = {
     networks: {
-        development: {
+        pippo: {
             host: "127.0.0.1",
             port: 7545,
             network_id: "*", // Match any network id
