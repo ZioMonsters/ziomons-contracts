6d772641057309c3189aef067cebee43fcecb8aa
Emanuele Caruso <ema.caruso2@gmail.com>
2018-09-08 20:38:50 +0000

    searching right contract

--- truffle.js
+++ truffle.js
@@ -1,8 +1,8 @@
 module.exports = {
     networks: {
-        default: {
+        pippo: {
             host: "localhost",
-            port: 1545,
+            port: 7545,
             network_id: "*", // Match any network id
             gas: 8000000,
             gasPrice: 1000
